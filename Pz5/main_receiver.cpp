4052e220749b452e87e899e77d47f77c29a41e7f
VadimZubarev <108616677+VadimZubarev@users.noreply.github.com>
2023-12-13 14:37:19 +0300

    Update main_receiver.cpp

--- Pz5/main_receiver.cpp
+++ Pz5/main_receiver.cpp
@@ -1,23 +1,29 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 
 int main() {
-    int sock;
-    struct sockaddr_in server_address, client_address;
+    int main_socket;
+    struct sockaddr_in saServer, caServer;
+    hostent* localHost;
+    char* localIP;
     char buffer[1024];
     
-    // Создаем сокет
     if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("Socket creation error");
+        perror("Socket haven't created!");
         exit(EXIT_FAILURE);
     }
     
-    // Заполняем структуру server_address
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(10000);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    localHost = gethostbyname("");
+    localIP = inet_ntoa (*(struct in_addr *)*localHost->h_addr_list);
+
+    saServer.sin_family = AF_INET;
+    saServer.sin_port = htons(5150);
+    saServer.sin_addr.s_addr = inet_addr(localIP);
     
     // Привязываем сокет к адресу и порту
     if (bind(sock, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
@@ -47,6 +53,66 @@ int main() {
         } else {
             printf("Received time is not equal to current time\n");
         }
+    }
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+
+int main() 
+{
+    int ListenSocket(AF_INET, SOCK_DGRAM, 0);
+    char buffer[1024];
+    struct sockaddr_in saServer;
+    hostent* localHost;
+    char* localIP;
+
+    if (ListenSocket < 0) 
+    {
+        perror("Socket haven't created!");
+        exit(EXIT_FAILURE);
+    }
+
+    localHost = gethostbyname("");
+    localIP = inet_ntoa (*(struct in_addr *)*localHost->h_addr_list);
+
+    saServer.sin_family = AF_INET;
+    saServer.sin_port = htons(5150);
+    saServer.sin_addr.s_addr = inet_addr(localIP);
+
+    bind( ListenSocket,(SOCKADDR*) &saServer, sizeof(saServer) );
+
+    while (1) 
+    {
+        time_t cur_time = time(NULL);
+        sprintf(buffer, "%ld", cur_time);
+        
+        sendto(ListenSocket, buffer, strlen(buffer), 0, (struct sockaddr *) &saServer, sizeof(saServer));
+        
+        sleep(2);
     }
     
     return 0;
